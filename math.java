5a79664edc586bf46a0ea26c93bdaddcfa26491e
pooja-71414 <pooja71414@gmail.com>
2023-12-29 16:07:25 +0530

    math

---
+++ math.java
@@ -0,0 +1,23 @@
+class math 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("10+20="+Math.addExact(10, 20));
+        System.out.println("10-20="+Math.subtractExact(10, 20));
+        System.out.println("sign(30)="+Math.multiplyExact(10, 20));
+        System.out.println("10/20="+Math.floorDiv(10, 20));
+        System.out.println("10+20="+Math.addExact(10, 20));
+        System.out.println("sin(30)="+Math.sin(30));
+        System.out.println("pow(2,3)="+Math.pow(2,3));
+        System.out.println("log(30)="+Math.log(30));
+        System.out.println("sin(30)="+Math.sin(30));
+        System.out.println("ceil(45.67)="+Math.ceil(45.67));
+        System.out.println("floor(45.67)="+Math.floor(45.67));
+        System.out.println("round(45.67)="+Math.round(45.67));
+        System.out.println("sqrt(25)="+Math.sqrt(25));
+        System.out.println("random="+Math.random());
+        System.out.println("pi="+Math.PI);
+        System.out.println("e="+Math.E);
+
+    }    
+}
