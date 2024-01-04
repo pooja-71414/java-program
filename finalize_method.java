825783f94d2d77fdbc53d39c664c1002acbb7309
pooja-71414 <pooja71414@gmail.com>
2024-01-04 11:59:07 +0530

    finalize

---
+++ finalize_method.java
@@ -0,0 +1,11 @@
+class finalize_method 
+{
+    protected void finalize()
+    {
+        System.out.println("finalized method called");
+    }
+    public static void main(String[] args) 
+    {
+
+    }
+}
