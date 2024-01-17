e5bfc49d3e588dcca4c7a24b9cdbaf375decb2f7
pooja-71414 <pooja71414@gmail.com>
2024-01-17 11:55:41 +0530

    primeornot

---
+++ THREAD.java
@@ -0,0 +1,28 @@
+class a extends thread
+{
+    for(int i=0;i<11;i++)
+    {
+        System.out.println("i = "+i);
+    }
+}
+class b 
+{
+    for(int j=0;j<11;j++)
+    {
+        System.out.println("j = "+j);
+    }
+}
+class c
+{
+    for(int k=0;k<11;k++)
+    {
+        System.out.println("k = "+k);
+    }
+}
+public class THREAD extends a
+{
+    public static void main(String[] args) 
+    {
+        a aa=new a();
+    }
+}
---
+++ prime_or_not.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class prime_or_not {
+    public static void main(String[] args) {
+        int i,j,a=0;
+        Scanner s=new Scanner(System.in);
+        System.out.print("enter the number = ");
+        i=s.nextInt();
+        for (j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                //System.out.println("not a prime number");
+                a=1;
+                break;
+            }
+        }
+        if(a==1)
+        {
+            System.out.println("not a prime number");   
+        }
+        else
+        {
+            System.out.println("prime number");   
+        }
+    }
+}
